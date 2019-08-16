0c58e507818af71a9b9302086ca3eca09aebb01d
Mark Garner <mgarner@outlook.com>
2019-08-16 16:03:24 -0500

    moved feed.xml

--- stripvideo.bat
+++ stripvideo.bat
@@ -1,5 +1,5 @@
 cd working
 del *.* /Q
 cd ..
-ffmpeg\ffmpeg -i "c:\users\mgarner\downloads\afn-090.mp4" -vn -c:a copy "working/afn-090.m4a"
-ffmpeg\ffprobe -v error -show_entries stream=duration -i working\afn-090.m4a -of default=noprint_wrappers=1:nokey=1 > working\length.out
+ffmpeg\ffmpeg -i "c:\users\mgarner\downloads\AFN-071.mp4" -vn -c:a copy "working/AFN-071.m4a"
+ffmpeg\ffprobe -v error -show_entries stream=duration -i working\AFN-071.m4a -of default=noprint_wrappers=1:nokey=1 > working\length.out
